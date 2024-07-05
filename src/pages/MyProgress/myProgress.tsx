577697841a4745eeee96182fe89f8ccbbd85d37a
dainshon <shon1107@naver.com>
2024-07-06 03:11:26 +0900

    Feat: css 수정

--- src/pages/MyProgress/myProgress.tsx
+++ src/pages/MyProgress/myProgress.tsx
@@ -24,20 +24,21 @@ const StyledText1 = styled.div`
 	justify-content: center;
 	flex-shrink: 0;
 	height: 40px;
+    align-items: center;
 `;
 
 const StyledText2 = styled.div`
-	display: flex;
-
-	width: 328px;
-	height: 30px;
-	flex-direction: column;
-	justify-content: center;
-	flex-shrink: 0;
+display: flex;
+flex-direction: row;
+width: 328px;
+height: 30px;
+justify-content: center;
+flex-shrink: 0;
+align-items: center;
 `;
 
 const StyledTprogress = styled.div`
-	width: 100%;
+    width: 100%;
 `;
 
 const Doughnut = styled.div`
@@ -54,31 +55,37 @@ const StyledNum = styled.div`
 `;
 
 export default function myProgress() {
-	const data = [
-		{ name: "도전중", value: 10, color: "#782EBF" },
-		{ name: "도전 성공", value: 20, color: "#8E9EE9" },
-		{ name: "도전 실패", value: 30, color: "#9D80E0" },
-	];
-	const test = 90;
+    const data = [
+        { name: '도전중', value: 10, color: '#782EBF' },
+        { name: '도전 성공', value: 20, color: '#8E9EE9' },
+        { name: '도전 실패', value: 30, color: '#9D80E0' },
+    ];
+    const test = 90;
+    const successNum = 11;
 
-	return (
-		<StyledTotal>
-			<StyledText1>
-				현재
-				<StyledNum>{test}</StyledNum>% 성공률로{" "}
-			</StyledText1>
-			<StyledText1>습관 형성중이에요!</StyledText1>
-			<StyledTprogress>
-				<progress id="progress" value="50" max="100" style={{ width: "50%", height: "30px", appearance: "none" }}>
-					70%
-				</progress>
-			</StyledTprogress>
-			<StyledText2>지금까지</StyledText2>
-			<StyledText2>11개의 습관을 형성했어요!</StyledText2>
+    return (
+        <StyledTotal>
+            <StyledText1>현재
+                <StyledNum>{test}</StyledNum>
+                % 성공률로
+            </StyledText1>
+            <StyledText1>습관 형성중이에요!</StyledText1>
+            <StyledTprogress>
 
-			<Doughnut>
-				<DoughnutChart data={data} />
-			</Doughnut>
-		</StyledTotal>
-	);
+                <progress id="progress" value="50" max="100" style={{ width: '50%', height: '30px', appearance: 'none' }}>
+                    70%
+                </progress>
+
+            </StyledTprogress>
+            <StyledText2>지금까지</StyledText2>
+            <StyledText2><StyledNum>{successNum}</StyledNum> 개의</StyledText2>
+            <StyledText2>습관을 형성했어요!</StyledText2>
+
+            <Doughnut>
+                <DoughnutChart data={data} />
+            </Doughnut>
+
+        </StyledTotal>
+    );
 }
+
