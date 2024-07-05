7a7e09e40b6428480090280f473abba8547defdc
dainshon <shon1107@naver.com>
2024-07-06 00:57:54 +0900

    Feat: addHabit페이지 디자인 수정

--- src/pages/MyProgress/myProgress.tsx
+++ src/pages/MyProgress/myProgress.tsx
@@ -4,13 +4,13 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import DoughnutChart from './DoughnutChart';
 
 interface DoughnutChartProps {
-  data: number[];
-  labels: string[];
-  backgroundColors: string[];
+    data: number[];
+    labels: string[];
+    backgroundColors: string[];
 }
 
-const StyledTotal= styled.div`
-width: 390px;
+const StyledTotal = styled.div`
+width: 100%;
 height: 844px;
 align-items: center;
 background-color: #65A55B;
@@ -48,25 +48,25 @@ export default function myProgress() {
         { name: '도전중', value: 10, color: '#782EBF' },
         { name: '도전 성공', value: 20, color: '#8E9EE9' },
         { name: '도전 실패', value: 30, color: '#9D80E0' },
-      ];
+    ];
 
-	return (
-    <StyledTotal>
-    <StyledText1>현재 90% 성공률로 </StyledText1>
-    <StyledText1>습관 형성중이에요!</StyledText1>
-    <StyledTprogress>
-    <progress id="progress" value="50" max="100"></progress>
-    </StyledTprogress>
+    return (
+        <StyledTotal>
+            <StyledText1>현재 90% 성공률로 </StyledText1>
+            <StyledText1>습관 형성중이에요!</StyledText1>
+            <StyledTprogress>
+                <progress id="progress" value="50" max="100"></progress>
+            </StyledTprogress>
 
 
 
-    <StyledText2>지금까지</StyledText2>
-    <StyledText2>11개의 습관을 형성했어요!</StyledText2>
+            <StyledText2>지금까지</StyledText2>
+            <StyledText2>11개의 습관을 형성했어요!</StyledText2>
 
-    <Doughnut>
-        <DoughnutChart data={data} />
-    </Doughnut>
+            <Doughnut>
+                <DoughnutChart data={data} />
+            </Doughnut>
 
-    </StyledTotal>
+        </StyledTotal>
     );
 }
