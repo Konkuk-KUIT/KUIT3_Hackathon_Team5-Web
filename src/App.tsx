ad0d408e01e9483da56365ec9fa0f0aadda1031f
kim jihwan <kjhwan0802@naver.com>
2024-07-05 14:45:10 +0900

    배포 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { styled } from "styled-components";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 const Button = styled.button`
 	background-color: green;
 `;
 
-function App() {
+function Test() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
+			<Link to={"/Home"}>Go to Home</Link>
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,6 +33,20 @@ function App() {
 			</div>
 			<div className="text-3xl font-bold underline">it goes Work!!</div>
 			<p className="read-the-docs">Click on the Vite and R eact logos to learn more</p>
+		</>
+	);
+}
+
+function App() {
+	const routes = [
+		{ path: "/", element: <Test /> },
+		{ path: "/Home", element: <HomePage /> },
+	];
+	const router = createBrowserRouter(routes, { basename: import.meta.env.DEV ? "/" : "/KUIT3_Hackathon_Team5-Web/" });
+
+	return (
+		<>
+			<RouterProvider router={router} />
 		</>
 	);
 }
