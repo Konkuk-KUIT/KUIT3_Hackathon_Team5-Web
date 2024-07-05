b81e116e9ebbecf48915408f833f6bb82a7e17c2
SeongYouKim <brian770@naver.com>
2024-07-05 23:45:27 +0900

    store 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import HabitDetailPage from "./pages/HabitDetailPage/HabitDetailPage";
 import FeedPage from "./pages/FeedPage/FeedPage";
+import StorePage from "./pages/StorePage/StorePage";
+import OptionPage from "./pages/OptionPage/OptionPage";
 
 function App() {
 	const routes = [
@@ -11,8 +13,8 @@ function App() {
 		{ path: "/feed", element: <FeedPage /> },
 		{ path: "/addHabit" },
 		{ path: "myprogress" },
-		{ path: "/store" },
-		{ path: "/option" },
+		{ path: "/store", element: <StorePage /> },
+		{ path: "/option", element: <OptionPage /> },
 	];
 	const router = createBrowserRouter(routes, { basename: "/KUIT3_Hackathon_Team5-Web/" });
 
